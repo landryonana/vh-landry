4d53ca61737412b539450994fae63f8ff9875021
landryonana <51939280+landryonana@users.noreply.github.com>
2021-08-13 07:22:58 +0100

    Update settings.py

--- vhproject2021/settings.py
+++ vhproject2021/settings.py
@@ -9,9 +9,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%hxpxexn^^@^nhmshmxn#*2le7$cya%s!81vc54m2=2@zx06$n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['vh-cam-yde.herokuapp.com', 'www']
 
 
 # Application definition
@@ -156,10 +156,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 django_heroku.settings(locals())
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': 'vh-cam',
