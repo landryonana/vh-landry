4e4470e9598b5b2607a943052a8c55a72cea40d4
landryonana <51939280+landryonana@users.noreply.github.com>
2021-08-13 07:47:18 +0100

    Update settings.py

--- vhproject2021/settings.py
+++ vhproject2021/settings.py
@@ -69,6 +69,7 @@ LOGIN_URL = 'accounts:user_login'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+     'whitenoise.middleware.WhiteNoiseMiddleware', # White Noise
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -157,9 +158,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 django_heroku.settings(locals())
 #STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': 'vh-cam',
